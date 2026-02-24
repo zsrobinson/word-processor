2282ebb1f5ed8787d05c5fb5ac36d2ee4df621e4
Zachary Robinson <me@zsrobinson.com>
2026-02-23 21:33:15 -0500

    add basic cursor

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ const ctx = canvas.getContext("2d");
 
 var text = "hello world!";
 var isFocused = false;
+var cursor = text.length;
 
 canvas.addEventListener("focus", () => { isFocused = true; });
 canvas.addEventListener("blur", () => { isFocused = false; });
 canvas.addEventListener("keydown", (e) => {
-    if (e.key === "Backspace") { text = text.slice(0, -1); }
-    else if (e.key === "Enter") { text += "\n"; }
+    if (e.key === "Backspace") { text = text.slice(0, -1); cursor -= 1; }
+    else if (e.key === "Enter") { text += "\n"; cursor += 1; }
     else if (e.key === "Shift") { /* no-op */ }
     else if (e.key === "Meta") { /* no-op */ }
-    else { text += e.key; } });
+    else { text += e.key; cursor += 1;} });
 
 function paint() {
     canvas.width = 850/2;
@@ -24,10 +25,27 @@ function paint() {
 
     // draw text
     ctx.font = "50px Arial";
-    ctx.fillStyle = "black";    
     const lines = text.split("\n");
+    var runningLetterCount = 0;
+
     for (let i = 0; i < lines.length; i++) {
-        ctx.fillText(lines[i], 10, 60 + (i * 50));
+        const lineHeight = 60 + (i * 50);
+        ctx.fillStyle = "black"; 
+        ctx.fillText(lines[i], 10, lineHeight);
+        
+        // draw cursor
+        if (cursor <= runningLetterCount + lines[i].length) {
+            const beforeCursor = cursor - runningLetterCount;
+            const textBeforeCursor = lines[i].split(0, beforeCursor);
+            const metrics = ctx.measureText(textBeforeCursor);
+
+            ctx.beginPath();
+            ctx.arc(metrics.width + 10, lineHeight, 2, 0, 2 * Math.PI);
+            ctx.fillStyle = "blue";
+            ctx.fill();
+        }
+
+        runningLetterCount += lines[i].length + 1;
     }
 
     // focus indicator
